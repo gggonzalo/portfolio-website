ac70f251d5560ae2803bc3af56cb1f7258c0bd25
Gonzalo <gonzalo.gonzalez.garro@gmail.com>
2023-02-17 09:05:48 -0600

    Fix sticky navbar bug caused by container height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: React.PropsWithChildren) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen max-w-4xl mx-auto flex flex-col">
+      <div className="min-h-screen max-w-4xl mx-auto flex flex-col">
         <Navbar />
         <main className="grow px-4 my-10">{children}</main>
         <Footer />
