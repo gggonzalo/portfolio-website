7a39b46e87d0114dcee44f4054257ae8de302c9c
Gonzalo <gonzalo.gonzalez.garro@gmail.com>
2023-03-06 09:08:07 -0600

    Move dark mode config of body to JSX

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="transition-[background-color] duration-500 bg-slate-100 dark:bg-slate-900">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
