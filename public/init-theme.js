12736d20e7eba14b67cc5fb18b645db61709999d
Gonzalo <gonzalo.gonzalez.garro@gmail.com>
2023-03-06 08:54:24 -0600

    Undo fix for flashing dark mode

--- public/init-theme.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  var theme = localStorage.getItem("theme") || "light";
-
-  if (theme === "dark") {
-    document.querySelector("html").classList.add("dark");
-  }
-})();
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,10 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Script src="/init-theme.js" strategy="beforeInteractive" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
